3683d5888f28205c35c061812578a96149fe143f
AnjalBam <anjalbam81@gmail.com>
2021-02-05 02:02:00 +0545

    [feat] setup files and project with static data

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
+
+const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+    const getData = async () => {
+        try {
+            const { data } = await axios.get("/api/products/");
+            setProducts(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {console.log(products)}
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
+};
+
+export default HomeScreen;
